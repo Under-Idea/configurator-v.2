626cc981ea33d5f1cf78a23bdcc440ba3af19374
UnderIdea <75729134+Under-Idea@users.noreply.github.com>
2023-02-01 18:14:03 +0100

    vite config change

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: '/configurator-v.2/'
+  base: '/configurator-v.2/',
+  plugins: [vue()]
 })
